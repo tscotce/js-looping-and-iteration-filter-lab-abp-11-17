3ebb3389f520e08373a6af6018df4e41ca2b8815
Tatyana Scotce <tscotce@gmail.com>
2017-12-18 23:04:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file
+function findMatching (drivers, name) {
+ return drivers.filter(function(driver){
+   if(name.toLowerCase () === driver.toLowerCase ()){
+     return true
+   }
+   else {
+     return false
+   }
+ })
+}
+
+function fuzzyMatch (drivers, name) {
+ let lengthOfName = name.length;
+  return drivers.filter (function (driver){
+    return driver.slice (0, lengthOfName) === name;
+  });
+}
+
+function matchName (drivers, name) {
+  return drivers.filter (function (driver) {
+    return driver.name.toLowerCase () === name.toLowerCase ();
+  });
+}
+
