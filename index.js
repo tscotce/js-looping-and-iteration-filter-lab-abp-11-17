55d84d70c62da337d63513c2385020b0c39a73f1
Tatyana Scotce <tscotce@gmail.com>
2017-12-18 22:50:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function findMatching (drivers, name) {
  return drivers.filter(function(driver){
-   if(name.toLowerCase === driver){
+   if(name.toLowerCase () === driver.toLowerCase ()){
      return true
    }
    else {
